5de4a95492d8b97624439b43926bee62b014e455
Sokos6 <will.sokolowski@gmail.com>
2019-08-02 14:22:24 -0700

    first todos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import Todo from './Todo';
 
 const App = () => (
   <div className="App">
     <h2>getDone</h2>
+    <br />
+    <Todo />
+    <Todo />
+    <Todo />
   </div>
 );
 
---
+++ src/Todo.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Todo.css';
+
+const Todo = () => <div className="Todo">I am an item</div>;
+
+export default Todo;
