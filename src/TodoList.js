42edc5694ba35f586bc4173966b5e1e83e836316
Sokos6 <will.sokolowski@gmail.com>
2019-08-02 15:28:39 -0700

    updated TodoList

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,14 +3,20 @@ import Todo from "./Todo";
 import "./TodoList.css";
 
 class TodoList extends Component {
-  render() {
-    return (
-      <div className="TodoList">
-        <Todo description="Item #1" />
-        <Todo description="Item #2" />
-      </div>
-    );
-  }
+
+constructor(props) {
+    super(props);
+    this.state = {items: ['Item #1', 'Item #2', 'Item #3'] };
 }
 
+renderItems() {
+    return this.state.items.map(description => (
+        <Todo key={description} description={description} />
+    ));
+}
+
+  render() {
+      return <div className="TodoList">{this.renderItems()}</div>
+  }
+}
 export default TodoList;
