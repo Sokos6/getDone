0476e0eedc2c171a73645671c42070b667f68fe3
Sokos6 <will.sokolowski@gmail.com>
2019-08-02 16:12:09 -0700

    todo interactivity

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,22 +1,33 @@
 import React, { Component } from "react";
 import Todo from "./Todo";
 import "./TodoList.css";
+import NewTodo from "./NewTodo";
 
 class TodoList extends Component {
-
-constructor(props) {
+  constructor(props) {
     super(props);
-    this.state = {items: ['Item #1', 'Item #2', 'Item #3'] };
-}
+    this.state = { items: ["Item #1", "Item #2", "Item #3"] };
 
-renderItems() {
+    this.addTodo = this.addTodo.bind(this);
+  }
+
+  addTodo(item) {
+    this.setState({ items: [...this.state.items, item] });
+  }
+
+  renderItems() {
     return this.state.items.map(description => (
-        <Todo key={description} description={description} />
+      <Todo key={description} description={description} />
     ));
-}
+  }
 
   render() {
-      return <div className="TodoList">{this.renderItems()}</div>
+    return (
+      <div className="TodoList">
+        <NewTodo addTodo={this.addTodo} />
+        {this.renderItems()}
+      </div>
+    );
   }
 }
 export default TodoList;
