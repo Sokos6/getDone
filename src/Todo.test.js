9de2ddc9297a139445fbb81d918c50b65f4fa80d
Sokos6 <will.sokolowski@gmail.com>
2019-08-02 18:57:07 -0700

    test and snapshot

--- src/Todo.test.js
+++ src/Todo.test.js
@@ -1,9 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Todo from "./Todo";
+import { shallow } from "enzyme";
+import renderer from "react-test-renderer";
 
-it("renders without crashing", () => {
+import Todo from "./Todo";
+import setupTests from "./setupTests"
+
+describe(Todo, () => {
+  const description = "New Todo";
+  const mockRemoveTodo = jest.fn();
+  const component = shallow(
+    <Todo description={description} removeTodo={mockRemoveTodo} />
+  );
+  it("renders without crashing", () => {
     const div = document.createElement("div");
     ReactDOM.render(<Todo />, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("renders and matches our snapshot", () => {
+    const component = renderer.create(<Todo description="Yo" />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
